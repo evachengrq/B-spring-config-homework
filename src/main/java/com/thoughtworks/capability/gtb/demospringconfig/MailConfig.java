c5a9577cf650b089e66520c7cec1140e72250dff
evachengrq <evachengrq@hotmail.com>
2020-11-18 00:13:27 +0800

    [evacheng]feat: finished practice 1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,42 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private Credentials credentials;
+    private List<String> defaultRecipients;
+    private Map<String,String> additionalHeaders;
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
+    }
+
+    public List<String> getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map<String, String> getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map<String, String> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
 
     public String getHostname() {
         return hostname;
@@ -39,6 +69,9 @@ public class MailConfig {
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", credentials=" + credentials +
+                ", defaultRecipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
                 '}';
     }
 }
